7546d5ffc216dd9c83271927e398e2ec69c40fcb
Maximillian-Udar <122749236+Maximillian-Udar@users.noreply.github.com>
2026-06-10 21:47:49 +0200

    Refactor PWM module to support variable frequency

--- Chisel/src/main/scala/DCMotorPWM.scala
+++ Chisel/src/main/scala/DCMotorPWM.scala
@@ -1,14 +1,20 @@
+
 import chisel3._
 import chisel3.util._
 
-class PwmFsmAnalogOut extends Module {
+class VariablePWM(pwmFreqHz: Int) extends Module {
   val io = IO(new Bundle {
     val switch_state = Input(Bool())
-    val pwmOut       = Output(Bool())
-    val pwmOutInv    = Output(Bool())
+    val pwmOutPos    = Output(Bool())
+    val pwmOutNeg    = Output(Bool())
+    
     val seg          = Output(UInt(8.W))
     val an           = Output(UInt(4.W))
   })
+
+  val clockFreq    = 100000000
+  val periodCycles = clockFreq / pwmFreqHz
+  val counterWidth = log2Ceil(periodCycles).W
 
   val input_debounce = Module(new Debouncer)
   val rising_edge    = Module(new RisingFsm)
@@ -20,7 +26,7 @@ class PwmFsmAnalogOut extends Module {
   object State extends ChiselEnum {
     val s10, s25, s50, s75, s90 = Value
   }
-  val stateReg = RegInit(State.s10)
+  val stateReg = RegInit(State.s50)
 
   when (rise) {
     switch (stateReg) {
@@ -31,62 +37,56 @@ class PwmFsmAnalogOut extends Module {
       is (State.s90) { stateReg := State.s10 }
     }
   }
-
-  val dutyCycle = Wire(UInt(12.W))
-  val d3        = Wire(UInt(5.W))
-  val d2        = Wire(UInt(5.W))
-  val d1        = Wire(UInt(5.W))
-  val d0        = Wire(UInt(5.W))
-
-  // Default Fallbacks
+    // Set default values as 0, 0, 0, 0, BLANK
+  val dutyCycle = Wire(UInt(counterWidth))
   dutyCycle := 0.U 
-  d3        := 0.U
-  d2        := 0.U
-  d1        := 0.U
-  d0        := 29.U
+  val d3        = WireInit(0.U(5.W))
+  val d2        = WireInit(0.U(5.W))
+  val d1        = WireInit(0.U(5.W))
+  val d0        = WireInit(29.U(5.W))
 
   switch (stateReg) {
     is (State.s10) { 
-      dutyCycle := 333.U
-      d2        := 1.U
-      d1        := 0.U
+      dutyCycle := (periodCycles * 10 / 100).U
+      d2 := 1.U
+      d1 := 0.U
     }
     is (State.s25) { 
-      dutyCycle := 833.U
-      d2        := 2.U
-      d1        := 5.U
+      dutyCycle := (periodCycles * 25 / 100).U
+      d2 := 2.U
+      d1 := 5.U
     }
     is (State.s50) { 
-      dutyCycle := 1667.U
-      d2        := 5.U
-      d1        := 0.U
+      dutyCycle := (periodCycles * 50 / 100).U
+      d2 := 5.U
+      d1 := 0.U
     }
     is (State.s75) { 
-      dutyCycle := 2500.U
-      d2        := 7.U
-      d1        := 5.U
+      dutyCycle := (periodCycles * 75 / 100).U
+      d2 := 7.U
+      d1 := 5.U
     }
     is (State.s90) { 
-      dutyCycle := 3000.U
-      d2        := 9.U
-      d1        := 0.U
+      dutyCycle := (periodCycles * 90 / 100).U
+      d2 := 9.U
+      d1 := 0.U
     }
   }
 
   val displayMux = Module(new DisplayMultiplexer)
-  
   displayMux.io.disp_content := Cat(d3, d2, d1, d0)
   displayMux.io.dots         := "b0100".U
-  
   io.seg := displayMux.io.seg
   io.an  := displayMux.io.an
 
-  val pwmCounter = RegInit(0.U(12.W))
-  when (pwmCounter === 3332.U) {
+  val pwmCounter = RegInit(0.U(counterWidth))
+  
+  when (pwmCounter === (periodCycles - 1).U) {
     pwmCounter := 0.U
   } .otherwise {
     pwmCounter := pwmCounter + 1.U
   }
-  io.pwmOut := pwmCounter < dutyCycle
-  io.pwmOutInv := ~(pwmCounter < dutyCycle)
+
+  io.pwmOutPos := pwmCounter < dutyCycle
+  io.pwmOutNeg := ~(pwmCounter < dutyCycle)
 }
